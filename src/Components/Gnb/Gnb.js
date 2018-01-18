c39e42d7999be562fe2060602c0a5094298f2541
joonie.lee <joonie.lee@samsung.com>
2018-01-18 18:18:33 +0900

    - gnb 스타일 변경 - README : Color Combo 추가

--- src/Components/Gnb/Gnb.js
+++ src/Components/Gnb/Gnb.js
@@ -9,14 +9,16 @@ import './Gnb.scss';
 const Gnb = () => {
 
     const activeStyle = {
-        color: 'green',
-        fontSize: '2rem'
+        // color: 'green'
+        backgroundColor: '#666'
+        // fontSize: '2rem'
     };
+    const homeStyle = {fontWeight: 'bold'};
 
     return(
         <div className="Gnb-gnb">
             <ul>
-                <li><NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink></li>
+                <li><NavLink exact to="/" className="gnb-home" activeStyle={activeStyle}>Home</NavLink></li>
                 <li><NavLink exact to="/about" activeStyle={activeStyle}>About</NavLink></li>
                 <li><NavLink to="/about/foo" activeStyle={activeStyle}>Project</NavLink></li>
                 <li><NavLink to="/posts" activeStyle={activeStyle}>Contact</NavLink></li>
--- src/shared/App.js
+++ src/shared/App.js
@@ -6,18 +6,21 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, About, Posts } from '../pages';
 import Gnb from '../components/Gnb/Gnb';
+import './App.scss';
 
 class App extends Component {
     render(){
         return(
             <div>
                 <Gnb/>
-                <Route exact path="/" component={Home}/>
-                <Switch>
-                    <Route path="/about/:name" component={About}/>
-                    <Route path="/about" component={About}/>
-                </Switch>
-                <Route path="/posts" component={Posts}/>
+                <section className="contents-wrap">
+                    <Route exact path="/" component={Home}/>
+                    <Switch>
+                        <Route path="/about/:name" component={About}/>
+                        <Route path="/about" component={About}/>
+                    </Switch>
+                    <Route path="/posts" component={Posts}/>
+                </section>
             </div>
         );
     }
